46742b85a00fc863322c0a79eaba7c0fca7ac81b
richas-bsingh <72738297+richas0719@users.noreply.github.com>
2024-02-13 17:51:57 -0500

    add calculator class with static methods and instance methods

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,19 +1,19 @@
 '''My Calculator Test'''
-from calculator import add, subtract, multiply, divide
+from calculator import Calculator
 
 def test_addition():
     '''Test that addition function works '''    
-    assert add(2,2) == 4
+    assert Calculator.add(2,2) == 4
 
 def test_subtraction():
     '''Test that substraction function works '''    
-    assert subtract(2,2) == 0
+    assert Calculator.subtract(2,2) == 0
     
 def test_multiplication():
     '''Test that multiply function works'''
-    assert multiply(2,2) == 4
+    assert Calculator.multiply(2,2) == 4
 
 def test_division():
     '''Test that division function works'''
-    assert divide(2,2) == 1
+    assert Calculator.divide(2,2) == 1
     
